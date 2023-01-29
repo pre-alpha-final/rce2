433a6ee5577f8cb2712a648c96b5c2d6231ccf2e
pre-alpha-final <jacek.mowinski@gmail.com>
2023-01-29 15:30:01 +0100

    Auth in UI

---
+++ broker/Broker/Client/Infrastructure/AppData.cs
@@ -0,0 +1,11 @@
+﻿using Broker.Shared.Model;
+
+namespace Broker.Client.Infrastructure;
+
+public class AppData
+{
+    public string Code { get; set; }
+    public bool AuthorizationFailure { get; set; }
+    public List<Agent> Agents { get; set; }
+    public List<Binding> Bindings { get; set; }
+}
