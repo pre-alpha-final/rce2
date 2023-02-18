ab4b67a0b0816e1b8675bb8743384cf46a3f8b6a
pre-alpha-final <jacek.mowinski@gmail.com>
2023-02-18 05:44:26 +0100

    Input simulation

--- broker/Broker/Server/Controllers/BrokerController.cs
+++ broker/Broker/Server/Controllers/BrokerController.cs
@@ -72,5 +72,31 @@ namespace Broker.Server.Controllers
 
             return Ok();
         }
+
+        [HttpPost("simulateIn/{id:Guid}")]
+        public async Task<OkResult> SimulateIn(Guid id, [FromBody] Rce2Message rce2Message)
+        {
+            await _agentFeedService.AddItem(id, rce2Message);
+
+            return Ok();
+        }
+
+        [HttpPost("simulateOut/{id:Guid}")]
+        public async Task<OkResult> SimulateOut(Guid id, [FromBody] Rce2Message rce2Message)
+        {
+            var bindings = _bindingRepository.GetBindingsFrom(id);
+            foreach (var binding in bindings)
+            {
+                rce2Message.Contact = binding.InContact;
+                await _agentFeedService.AddItem(binding.InId, rce2Message);
+            }
+            await _brokerFeedService.BroadcastItem(new AgentOutputEvent
+            {
+                AgentId = id,
+                Payload = rce2Message.Payload,
+            });
+
+            return Ok();
+        }
     }
 }
