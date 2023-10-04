ef6129aded1480fd2ae07e397f54f0803b2d6e3b
pre-alpha-final <jacek.mowinski@gmail.com>
2023-10-04 20:58:14 +0200

    Rce2 C# minimal boilerplate

---
+++ agents/_MinimalBoilerplateCSharp/Rce2/Infrastructure/Rce2Contacts.cs
@@ -0,0 +1,14 @@
+namespace Rce2;
+
+public class Rce2Contacts
+{
+    public class Ins
+    {
+        public const string ExampleInput = "example-input";
+    }
+
+    public class Outs
+    {
+        public const string ExampleOutput = "example-output";
+    }
+}
---
+++ agents/_MinimalBoilerplateCSharp/Rce2/Infrastructure/Rce2Message.cs
@@ -0,0 +1,10 @@
+using Newtonsoft.Json.Linq;
+
+namespace Rce2;
+
+public class Rce2Message
+{
+    public string Type { get; set; }
+    public string? Contact { get; set; }
+    public JToken Payload { get; set; }
+}
