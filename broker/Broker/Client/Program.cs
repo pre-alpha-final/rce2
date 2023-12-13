c6e120e48030399b527f738cd8bb9a75148b2996
pre-alpha-final <jacek.mowinski@gmail.com>
2023-12-13 06:54:26 +0100

    Using HttpClientFactory

--- broker/Broker/Client/Program.cs
+++ broker/Broker/Client/Program.cs
@@ -1,11 +1,22 @@
 using Broker.Client;
+using Broker.Client.Infrastructure;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+        builder.Services.AddHttpClient("Default", httpClient =>
+        {
+            httpClient.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
+        });
+        builder.Services.AddSingleton<AuthorizedHttpClientFactory>();
 
-await builder.Build().RunAsync();
+        await builder.Build().RunAsync();
+    }
+}
