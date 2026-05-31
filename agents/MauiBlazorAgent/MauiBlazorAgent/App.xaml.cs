6fbaf5a00a6cbbed0699d74abb8ccd7894c9b4a7
pre-alpha-final <jacek.mowinski@gmail.com>
2026-05-31 18:32:41 +0200

    Base scaffold

---
+++ agents/MauiBlazorAgent/MauiBlazorAgent/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace MauiBlazorAgent
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage()) { Title = "MauiBlazorAgent" };
+        }
+    }
+}
---
+++ agents/MauiBlazorAgent/MauiBlazorAgent/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiBlazorAgent
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
