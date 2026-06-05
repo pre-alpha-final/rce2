a7ecb2bff60e61a92f5f23ee41103120eaad3396
pre-alpha-final <jacek.mowinski@gmail.com>
2026-06-06 00:18:09 +0200

    Removing ForceAgentAuth

--- broker/Broker/Server/Services/Implementation/AgentKeyService.cs
+++ broker/Broker/Server/Services/Implementation/AgentKeyService.cs
@@ -2,26 +2,15 @@
 
 public class AgentKeyService : IAgentKeyService
 {
-    private readonly IConfiguration _configuration;
     private Dictionary<Guid, string> _agentKeys { get; set; } = new();
-
-    public AgentKeyService(IConfiguration configuration)
-    {
-        _configuration = configuration;
-    }
 
     public async Task<bool> Validate(Guid agentId, string? agentKey)
     {
-        if (_agentKeys.ContainsKey(agentId) && agentKey == _agentKeys[agentId])
+        if (_agentKeys.ContainsKey(agentId))
         {
-            return true;
+            return agentKey == _agentKeys[agentId];
         }
 
-        if (string.IsNullOrWhiteSpace(agentKey) && Convert.ToBoolean(_configuration["ForceAgentAuth"]) == false)
-        {
-            return true;
-        }
-
-        return false;
+        return true;
     }
 }
